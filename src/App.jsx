3d23621b0e0ac4a13aa559cd800812faf6cfdde3
ExiledVibe <32912837+ExiledVibe@users.noreply.github.com>
2025-05-29 16:08:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import "@fullcalendar/daygrid/index.css";
-import "@fullcalendar/timegrid/index.css";
+import "./index.css";
 import "./App.css";
 
 const App = () => {
