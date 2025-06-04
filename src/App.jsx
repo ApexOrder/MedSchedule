2b101d480814f54314f7758740d2bbbcdc41b150
ExiledVibe <32912837+ExiledVibe@users.noreply.github.com>
2025-06-04 11:42:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import hexToRgb from "../utils/hexToRgb"; // or paste your helper inline
+import hexToRgb from "/utils/hexToRgb"; // or paste your helper inline
 
 const CalendarWrapper = ({
   events = [],
