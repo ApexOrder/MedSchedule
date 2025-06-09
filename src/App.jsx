22bcc9b96f048c33b6b08bf7a9048d3c08dcdfeb
ExiledVibe <32912837+ExiledVibe@users.noreply.github.com>
2025-06-09 15:25:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import TagManager from "./components/TagManager";
 import EventModal from "./components/EventModal";
 import ConfirmDialog from "./components/ConfirmDialog";
 import CalendarWrapper from "./components/CalendarWrapper";
-import TodaysEventsModal from "./src/TodaysEventsModal";
+import TodaysEventsModal from "./components/TodaysEventsModal";
 
 import hexToRgb from "./utils/hexToRgb";
 import "./App.css";
